d35695b92e7751fe2dcf8fd98c118df4dabe75ab
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-04-23 07:58:09 +0800

    feat: Create CLI application

---
+++ bin/06_two_heaps/04_schedule_tasks_on_minimum_machines.dart
@@ -0,0 +1,17 @@
+// @dart = 3.0
+// dart pub add collection (This is just a comment, but see below)
+
+import 'package:collection/collection.dart';
+
+void main() {
+  int minimumMachines(List<List<int>> tasks) {}
+  // CASE 1
+  final tasks1 = [
+    [1, 7],
+    [8, 13],
+    [5, 6],
+    [10, 14],
+    [6, 7]
+  ];
+  print(minimumMachines(tasks1));
+}
