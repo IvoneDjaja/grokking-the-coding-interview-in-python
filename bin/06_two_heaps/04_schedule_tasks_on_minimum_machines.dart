64a3a84a3c403a826a29c93175ca1279d4248d92
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-04-25 08:23:29 +0800

    feat(two_heaps): Schedule tasks on minimum machines iteration 0 with error on case 6

--- bin/06_two_heaps/04_schedule_tasks_on_minimum_machines.dart
+++ bin/06_two_heaps/04_schedule_tasks_on_minimum_machines.dart
@@ -4,7 +4,45 @@
 import 'package:collection/collection.dart';
 
 void main() {
-  int minimumMachines(List<List<int>> tasks) {}
+  int minimumMachines(List<List<int>> tasks) {
+    final minStartHeap = HeapPriorityQueue<List<int>>((a, b) {
+      if (a.first < b.first) {
+        return -1;
+      } else if (a.first == b.first) {
+        return 0;
+      } else {
+        return 1;
+      }
+    });
+    final minEndHeap = HeapPriorityQueue<List<int>>((a, b) {
+      if (a.last < b.last) {
+        return -1;
+      } else if (a.last == b.last) {
+        return 0;
+      } else {
+        return 1;
+      }
+    });
+
+    minStartHeap.addAll(tasks);
+
+    var time = 0;
+    while (minStartHeap.isNotEmpty) {
+      final task = minStartHeap.removeFirst();
+
+      minEndHeap.add(task);
+      time = task.first;
+      while (minEndHeap.isNotEmpty &&
+          (minEndHeap.first.last <= time ||
+              (minStartHeap.isNotEmpty &&
+                  minEndHeap.first.last < minStartHeap.first.first))) {
+        minEndHeap.removeFirst();
+      }
+    }
+
+    return minEndHeap.length;
+  }
+
   // CASE 1
   final tasks1 = [
     [1, 7],
@@ -14,4 +52,55 @@ void main() {
     [6, 7]
   ];
   print(minimumMachines(tasks1));
+
+  // CASE 2
+  final tasks2 = [
+    [2, 5],
+    [2, 5],
+    [2, 5],
+    [2, 5],
+  ];
+  print(minimumMachines(tasks2));
+
+  // CASE 3
+  final tasks3 = [
+    [2, 3],
+    [4, 7],
+    [8, 18],
+    [19, 25],
+    [26, 30],
+  ];
+  print(minimumMachines(tasks3));
+
+  // CASE 4
+  final tasks4 = [
+    [12, 13],
+    [13, 15],
+    [17, 20],
+    [13, 14],
+    [19, 21],
+    [18, 20]
+  ];
+  print(minimumMachines(tasks4));
+
+  // CASE 5
+  final tasks5 = [
+    [1, 4],
+    [3, 5],
+    [6, 8],
+    [7, 8],
+  ];
+  print(minimumMachines(tasks5));
+
+  // CASE 6
+  final tasks6 = [
+    [1, 3],
+    [3, 5],
+    [5, 9],
+    [9, 12],
+    [12, 13],
+    [13, 16],
+    [16, 17]
+  ];
+  print(minimumMachines(tasks6));
 }
