af56cdd11061c3b2dcbd737ef8e554c7fcb561b9
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-01 20:04:25 +0800

    feat(k_way_merge): Kth smallest element in a sorted matrix iteration 0 with case 1 failing

---
+++ bin/07_introduction_to_k_way_merge/02_kth_smallest_element_in_a_sorted_matrix.dart
@@ -0,0 +1,27 @@
+import 'package:collection/collection.dart';
+
+void main() {
+  int kthSmallestElement(List<List<int>> matrix, int k) {
+    final maxHeap = HeapPriorityQueue<int>((a, b) => b.compareTo(a));
+
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix[0].length; j++) {
+        if (maxHeap.length == k) {
+          return maxHeap.first;
+        }
+        maxHeap.add(matrix[i][j]);
+      }
+    }
+
+    return -1;
+  }
+
+  /// CASE 1
+  final matrix1 = [
+    [2, 6, 8],
+    [3, 6, 10],
+    [5, 8, 11],
+  ];
+  final k1 = 5;
+  print(kthSmallestElement(matrix1, k1));
+}
