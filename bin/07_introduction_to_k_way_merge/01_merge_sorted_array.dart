5243a673a7a7f5dfa6e12f0e65f8ebfe706114b3
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-01 19:37:24 +0800

    feat(k_way_merge): Merge sorted array iteration 2

--- bin/07_introduction_to_k_way_merge/01_merge_sorted_array.dart
+++ bin/07_introduction_to_k_way_merge/01_merge_sorted_array.dart
@@ -2,12 +2,33 @@ import 'package:collection/collection.dart';
 
 void main() {
   List<int> mergeSorted(List<int> nums1, int m, List<int> nums2, int n) {
-    final minHeap = HeapPriorityQueue<int>((a, b) => a.compareTo(b));
-
-    minHeap.addAll(nums1.slice(0, m));
-    minHeap.addAll(nums2);
-
-    return minHeap.toList();
+    var index = m + n - 1;
+    var index1 = m - 1;
+    var index2 = n - 1;
+    while (index > -1) {
+      if (index1 < 0 || index2 < 0) {
+        break;
+      }
+      if (nums1[index1] > nums2[index2]) {
+        nums1[index] = nums1[index1];
+        index1 -= 1;
+      } else if (nums1[index1] <= nums2[index2]) {
+        nums1[index] = nums2[index2];
+        index2 -= 1;
+      }
+      index -= 1;
+    }
+    while (index1 > -1) {
+      nums1[index] = nums1[index1];
+      index1 -= 1;
+      index -= 1;
+    }
+    while (index2 > -1) {
+      nums1[index] = nums2[index2];
+      index2 -= 1;
+      index -= 1;
+    }
+    return nums1;
   }
 
   /// CASE 1
