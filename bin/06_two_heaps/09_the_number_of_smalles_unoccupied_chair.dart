cd445f2eafb0459bea4a771d359847834384b49b
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-01 14:11:13 +0800

    feat(two_heaps): The number of the smallest unoccupied chair retry 1 with error on case 6, 7

--- bin/06_two_heaps/09_the_number_of_smalles_unoccupied_chair.dart
+++ bin/06_two_heaps/09_the_number_of_smalles_unoccupied_chair.dart
@@ -1,36 +1,39 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 
+/// Retry: 1
 void main() {
   int smallestChair(List<List<int>> times, int targetFriend) {
-    final targetArrival = times[targetFriend][0];
+    final targetTime = times[targetFriend];
+
     times.sort((a, b) => a.first.compareTo(b.first));
 
-    final emptyChairs = HeapPriorityQueue<int>((a, b) => a.compareTo(b));
-    for (int i = 0; i < times.length; i++) {
-      emptyChairs.add(i);
+    final startTimes = [];
+    for (var i = 0; i < times.length; i++) {
+      startTimes.add([times[i][0], times[i][1], i]);
     }
 
-    final occupied = HeapPriorityQueue<List<int>>(
-      (a, b) => a.first.compareTo(b.first),
+    final minEndHeap = HeapPriorityQueue<List<int>>(
+      (a, b) => a[1].compareTo(b[1]),
     );
 
-    for (final time in times) {
-      final arrival = time.first;
-      final leave = time.last;
-
-      while (occupied.isNotEmpty && occupied.first.first <= arrival) {
-        emptyChairs.add(occupied.removeFirst().last);
+    final startTime = startTimes.first;
+    minEndHeap.add([startTime[0], startTime[1], 0]);
+    for (var i = 1; i < times.length; i++) {
+      final time = times[i];
+      var smallest = i;
+      if (minEndHeap.isNotEmpty && minEndHeap.first[1] <= time[0]) {
+        final pop = minEndHeap.removeFirst();
+        smallest = min(smallest, pop.last);
       }
-
-      final currentChair = emptyChairs.removeFirst();
-
-      if (arrival == targetArrival) {
-        return currentChair;
+      if (time.first == targetTime.first) {
+        return smallest;
       }
-
-      occupied.add([leave, currentChair]);
+      minEndHeap.add([time[0], time[1], smallest]);
     }
-    return 0;
+
+    return -1;
   }
 
   // CASE 1
@@ -62,4 +65,46 @@ void main() {
   ];
   final targetFriend3 = 0;
   print(smallestChair(times3, targetFriend3));
+
+  // CASE 4
+  final times4 = [
+    [9, 16],
+    [16, 21],
+    [21, 24],
+    [24, 30],
+    [30, 40],
+    [40, 44],
+    [44, 54],
+  ];
+  final targetFriend4 = 3;
+  print(smallestChair(times4, targetFriend4));
+
+  // CASE 5
+  final times5 = [
+    [1, 5],
+    [6, 7],
+    [8, 16],
+    [17, 19],
+    [20, 25],
+    [26, 31],
+    [32, 39],
+  ];
+  final targetFriend5 = 4;
+  print(smallestChair(times5, targetFriend5));
+
+  // CASE 6
+  final times6 = [
+    [88734, 94568],
+    [47668, 55499],
+    [8353, 55582],
+  ];
+  final targetFriend6 = 0;
+  print(smallestChair(times6, targetFriend6));
+
+  // CASE 7
+  final times7 = [
+    [2, 4],
+  ];
+  final targetFriend7 = 0;
+  print(smallestChair(times7, targetFriend7));
 }
