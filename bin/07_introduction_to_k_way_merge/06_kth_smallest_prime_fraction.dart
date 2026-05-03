45ef20cc684607e18a6b28c176ba171334395366
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-03 16:44:01 +0800

    feat(k_way_merge): Kth smallest prime fraction iteration 0 with failing case 2 in python because it is sorting numerator without denominator

--- bin/07_introduction_to_k_way_merge/06_kth_smallest_prime_fraction.dart
+++ bin/07_introduction_to_k_way_merge/06_kth_smallest_prime_fraction.dart
@@ -2,24 +2,35 @@ import 'package:collection/collection.dart';
 
 void main() {
   List<int> kthSmallestPrimeFraction(List<int> arr, int k) {
-    late List<int> result;
     final minHeap = HeapPriorityQueue<List<int>>(
       (a, b) => (a.first / a[1]).compareTo(b.first / b[1]),
     );
 
     for (var i = 0; i < arr.length; i++) {
-      for (var j = arr.length - 1; j > i; j--) {
-        minHeap.add([arr[i], arr[j], i, j]);
-        if (minHeap.length == k) {
-          result = [arr[i], arr[j]];
-        }
+      minHeap.add([arr[i], arr[arr.length - 1], i, arr.length - 1]);
+    }
+
+    for (var i = 0; i < k - 1; i++) {
+      final smallest = minHeap.removeFirst();
+      final row = smallest[2];
+      final col = smallest.last;
+
+      if (col - 1 > row) {
+        minHeap.add([arr[row], arr[col - 1], row, col - 1]);
       }
     }
-    return result;
+
+    final smallest = minHeap.removeFirst();
+    return [smallest.first, smallest[1]];
   }
 
   /// CASE 1
   final arr1 = [1, 2, 3, 5, 7];
   final k1 = 4;
   print(kthSmallestPrimeFraction(arr1, k1));
+
+  /// CASE 2
+  final arr2 = [1, 2, 3, 5];
+  final k2 = 3;
+  print(kthSmallestPrimeFraction(arr2, k2));
 }
