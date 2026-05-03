ee8f829af4eb0e535f1cc889f27b681626fd0888
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-04 07:54:29 +0800

    feat(modified_binary_search): Binary search iteraion 0 with failing case 2

---
+++ bin/09_modified_binary_search/01_binary_search.dart
@@ -0,0 +1,29 @@
+void main() {
+  int binarySearch(List<int> nums, int target) {
+    var low = 0;
+    var high = nums.length - 1;
+
+    while (low < high) {
+      final mid = (low + high) ~/ 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return -1;
+  }
+
+  /// CASE 1
+  final nums1 = [-5, 0, 2, 6, 12];
+  final target1 = 6;
+  print(binarySearch(nums1, target1));
+
+  /// CASE 2
+  final nums2 = [1, 6, 8, 10];
+  final target2 = 1;
+  print(binarySearch(nums2, target2));
+}
