7206e2c97293f972e68edd0adea4a257d09f5974
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-03 22:33:33 +0800

    feat(k_way_merge): Reorganize string iteration 0 with failing case 1

---
+++ bin/08_top_k_elements/02_reorganize_string.dart
@@ -0,0 +1,46 @@
+import 'package:collection/collection.dart';
+
+void main() {
+  String reorganizeString(String str) {
+    var output = '';
+
+    Map<String, int> countMap = <String, int>{};
+
+    for (var i = 0; i < str.length; i++) {
+      final char = str[i];
+      countMap[char] = (countMap[char] ?? 0) + 1;
+    }
+
+    final maxHeap = HeapPriorityQueue<(String, int)>(
+      (a, b) => b.$2.compareTo(a.$2),
+    );
+    for (var entry in countMap.entries) {
+      maxHeap.add((entry.key, entry.value));
+    }
+
+    while (maxHeap.isNotEmpty) {
+      final firstChar = maxHeap.removeFirst();
+      if (output.isNotEmpty && output[output.length - 1] == firstChar.$1) {
+        if (maxHeap.isEmpty) {
+          return '';
+        }
+        final secondChar = maxHeap.removeFirst();
+        output = '$output${secondChar.$1}';
+        if (secondChar.$2 - 1 > 0) {
+          maxHeap.add((secondChar.$1, secondChar.$2 - 1));
+        }
+      } else {
+        output = '$output${firstChar.$1}';
+      }
+      if (firstChar.$2 - 1 > 0) {
+        maxHeap.add((firstChar.$1, firstChar.$2 - 1));
+      }
+    }
+
+    return output;
+  }
+
+  /// CASE 1
+  final input1 = 'aaabc';
+  print(reorganizeString(input1));
+}
