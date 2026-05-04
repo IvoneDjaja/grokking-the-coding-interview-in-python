bf505959b05ccfa105dfadcda7b8add6f4090925
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-05 07:32:59 +0800

    feat(modified_binary_search): Search in rotated sorted array iteration 0 with failing case 4, 5

--- bin/09_modified_binary_search/02_search_in_rotated_sorted_array.dart
+++ bin/09_modified_binary_search/02_search_in_rotated_sorted_array.dart
@@ -7,14 +7,14 @@ void main() {
       final mid = (low + high) ~/ 2;
       if (nums[mid] == target) {
         return mid;
-      } else if (nums[low] < nums[high]) {
-        if (nums[mid] > target) {
+      } else if (nums[mid] > target) {
+        if (nums[low] < nums[mid]) {
           high = mid - 1;
         } else {
           low = mid + 1;
         }
       } else {
-        if (nums[mid] > target) {
+        if (nums[mid] < nums[high]) {
           low = mid + 1;
         } else {
           high = mid - 1;
@@ -34,4 +34,14 @@ void main() {
   final nums2 = [176, 188, 199, 200, 1, 2, 3];
   final target2 = 199;
   print(binarySearchRotated(nums2, target2));
+
+  /// CASE 3
+  final nums3 = [1, 2, 3, 4, 5, 6, 7];
+  final target3 = 8;
+  print(binarySearchRotated(nums3, target3));
+
+  /// CASE 4
+  final nums4 = [6, 7, 1, 2, 3, 4, 5];
+  final target4 = 6;
+  print(binarySearchRotated(nums4, target4));
 }
