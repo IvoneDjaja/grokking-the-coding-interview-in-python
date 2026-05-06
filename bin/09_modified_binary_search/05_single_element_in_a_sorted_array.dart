3855bdb6b53ef5092826a571a9168a312dd76d6a
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-06 08:06:39 +0800

    feat(modified_binary_search): Single element in a sorted array iteration 0 with failing case 3

--- bin/09_modified_binary_search/05_single_element_in_a_sorted_array.dart
+++ bin/09_modified_binary_search/05_single_element_in_a_sorted_array.dart
@@ -4,21 +4,23 @@ void main() {
     var high = nums.length - 1;
 
     while (low <= high) {
-      print('while');
-      final mid = (low + high) ~/ 2;
-      print('low: $low');
-      print('high: $high');
-      print('mid: $mid');
-      if (((mid - 1 > low && nums[mid] != nums[mid - 1]) || mid - 1 < 0) &&
-          ((mid + 1 < high && nums[mid] != nums[mid + 1]) || mid + 1 >= high)) {
-        print('if');
-        return mid;
-      } else if (mid - 1 > low && nums[mid] == nums[mid - 1]) {
-        print('else if 1');
-        low = mid + 1;
+      final mid = low + ((high - low) ~/ 2);
+      if ((mid - 1 < 0 || nums[mid] != nums[mid - 1]) &&
+          ((mid + 1 > nums.length || nums[mid] != nums[mid + 1]) ||
+              mid + 1 >= high)) {
+        return nums[mid];
+      }
+      int leftSize;
+      if (nums[mid - 1] == nums[mid]) {
+        leftSize = mid - 1;
       } else {
-        print('else if 2');
+        leftSize = mid;
+      }
+
+      if (leftSize % 2 == 1) {
         high = mid - 1;
+      } else {
+        low = mid + 1;
       }
     }
 
@@ -28,4 +30,12 @@ void main() {
   /// CASE 1
   final arr1 = [1, 1, 2, 3, 3, 4, 4];
   print(singleNonDuplicate(arr1));
+
+  /// CASE 2
+  final arr2 = [1, 1, 2, 2, 3, 3, 4];
+  print(singleNonDuplicate(arr2));
+
+  /// CASE 3
+  final arr3 = [1, 2, 2, 3, 3, 4, 4];
+  print(singleNonDuplicate(arr3));
 }
