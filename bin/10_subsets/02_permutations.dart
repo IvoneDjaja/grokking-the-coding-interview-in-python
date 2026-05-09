bf132d79de82f3e2df7e7376b066318d23a4e841
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-09 17:52:00 +0800

    feat(subsets): Permutations iteration 3

--- bin/10_subsets/02_permutations.dart
+++ bin/10_subsets/02_permutations.dart
@@ -1,22 +1,40 @@
 void main() {
-  List<String> permuteWord(String word) {
+  List<String> permuteWord1(String word) {
     if (word.isEmpty) {
       return [''];
     }
 
-    final perms = permuteWord(word.substring(1));
+    final perms = permuteWord1(word.substring(1));
     final result = <String>[];
     for (var perm in perms) {
       for (var i = 0; i < perm.length + 1; i++) {
-        final perm_copy =
+        final permCopy =
             '${perm.substring(0, i)}${word[0]}${perm.substring(i)}';
-        result.add(perm_copy);
+        result.add(permCopy);
       }
     }
 
     return result;
   }
 
+  List<String> permuteWord2(String word) {
+    var perms = [''];
+    for (var j = 0; j < word.length; j++) {
+      final newPerms = <String>[];
+      for (var perm in perms) {
+        for (var i = 0; i < perm.length + 1; i++) {
+          final permCopy =
+              '${perm.substring(0, i)}${word[j]}${perm.substring(i)}';
+          newPerms.add(permCopy);
+        }
+      }
+      perms = newPerms;
+    }
+
+    return perms;
+  }
+
   /// CASE 1
-  print(permuteWord('bad'));
+  print(permuteWord1('bad'));
+  print(permuteWord2('bad'));
 }
