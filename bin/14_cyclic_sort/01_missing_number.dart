f9b85da41b3ffb41e58cfd8a2a23b7288a758cde
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-19 00:32:50 +0800

    feat(cyclic_sort): Missing number iteration 0 with failing case 4

--- bin/14_cyclic_sort/01_missing_number.dart
+++ bin/14_cyclic_sort/01_missing_number.dart
@@ -2,28 +2,47 @@ void main() {
   int findMissingNumber(List<int> nums) {
     for (var num in nums) {
       final i = num.abs() - 1;
-      if (i > 0) {
-        nums[i] = -1 * nums[i];
+      if (i >= 0 && i < nums.length) {
+        if (nums[i] > 0) {
+          nums[i] = -1 * nums[i];
+        }
       }
     }
 
-    print(nums);
-
-    var result = 0;
     for (var i = 0; i < nums.length; i++) {
       if (nums[i] > 0) {
-        result = i + 1;
+        return i + 1;
       }
     }
 
-    return result;
+    return nums.length + 1;
   }
 
   /// CASE 1
-  final nums2 = [5, 6, 4, 2, 1, 3, 0, 7, 9];
-  print(findMissingNumber(nums2));
+  final nums1 = [5, 6, 4, 2, 1, 3, 0, 7, 9];
+print(findMissingNumber(nums1));
 
   /// CASE 2
-  final nums1 = [5, 6, 10, 2, 1, 3, 0, 7, 9, 12, 11, 8];
-  print(findMissingNumber(nums1));
+  final nums2 = [5, 6, 10, 2, 1, 3, 0, 7, 9, 12, 11, 8];
+  print(findMissingNumber(nums2));
+
+  /// CASE 3
+  final nums3 = [0, 1, 2, 4];
+  print(findMissingNumber(nums3));
+
+  /// CASE 4
+  final nums4 = [3, 0, 1, 4];
+  print(findMissingNumber(nums4));
+
+  /// CASE 5
+  final nums5 = [1, 4, 5, 6, 8, 2, 0, 7];
+  print(findMissingNumber(nums5));
+
+  /// CASE 6
+  final nums6 = [1, 0, 2, 3, 4, 5, 6, 8, 9, 7, 11];
+  print(findMissingNumber(nums6));
+
+  /// CASE 7
+  final nums7 = [1];
+  print(findMissingNumber(nums7));
 }
