2faae2b6bec533f2435bd6c40affc6477a986a3b
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-19 19:23:53 +0800

    feat(cyclic_sort): Sort array by parity 2 iteration 1

--- bin/14_cyclic_sort/05_sort_array_by_parity_2.dart
+++ bin/14_cyclic_sort/05_sort_array_by_parity_2.dart
@@ -1,30 +1,19 @@
 void main() {
   List<int> sortArrayByParity(List<int> nums) {
     final n = nums.length;
-    var i = 0;
-    while (i < n) {
-      if (i % 2 == 0 && nums[i] % 2 != 0) {
-        var j = i;
-        while (j < n) {
-          if (nums[j] % 2 == 0) {
-            final temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-          }
-          j += 1;
-        }
-      } else if (i % 2 == 1 && nums[i] % 2 != 1) {
-        var j = i;
-        while (j < n) {
-          if (nums[j] % 2 == 1) {
-            final temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-          }
-          j += 1;
-        }
+    var even = 0;
+    var odd = 1;
+    while (even < n && odd < n) {
+      if (nums[even] % 2 == 0) {
+        even += 2;
+      } else if (nums[odd] % 2 != 0) {
+        odd += 2;
       } else {
-        i += 1;
+        final temp = nums[even];
+        nums[even] = nums[odd];
+        nums[odd] = temp;
+        even += 2;
+        odd += 2;
       }
     }
     return nums;
@@ -41,4 +30,8 @@ void main() {
   /// CASE 3
   final input3 = [1, 2];
   print(sortArrayByParity(input3));
+
+  /// CASE 4
+  final input4 = [1, 2, 3, 4, 5, 6];
+  print(sortArrayByParity(input4));
 }
