ea98bcf900ad953b3792eba7baccb11c358c5528
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-27 14:34:46 +0800

    feat(stacks): Decode string iteration 2

--- bin/18_stacks/08_decode_string.dart
+++ bin/18_stacks/08_decode_string.dart
@@ -1,28 +1,32 @@
 void main() {
   String decodeString(String s) {
-    var stack = [];
+    final countStack = [];
+    final stringStack = [];
+    var currentString = StringBuffer();
+    var currentCount = 0;
 
     for (var i = 0; i < s.length; i++) {
-      if (s[i] != ']') {
-        stack.add(s[i]);
-      } else {
-        // get string
-        var string = '';
-        while (stack[stack.length - 1] != '[') {
-          string = stack.removeLast() + string;
-        }
-        stack.removeLast();
+      final char = s[i];
+      final codeUnit = char.codeUnitAt(0);
 
-        var countString = '';
-        while (stack.isNotEmpty &&
-            RegExp(r'^[0-9]').hasMatch(stack[stack.length - 1])) {
-          countString = stack.removeLast() + countString;
-        }
-        var count = int.parse(countString);
-        stack.add(string * count);
+      if (codeUnit >= 48 && codeUnit <= 57) {
+        currentCount = (currentCount * 10) + (codeUnit - 48);
+      } else if (char == '[') {
+        countStack.add(currentCount);
+        stringStack.add(currentString);
+
+        currentString = StringBuffer();
+        currentCount = 0;
+      } else if (char == ']') {
+        final count = countStack.removeLast();
+        final decoded = currentString.toString() * count;
+        currentString = stringStack.removeLast();
+        currentString.write(decoded);
+      } else {
+        currentString.write(char);
       }
     }
-    return stack.join('');
+    return currentString.toString();
   }
 
   /// CASE 1
