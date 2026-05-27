498e9b40744b1ea5c473bd7ebc9591cd8e96fea4
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-27 09:06:42 +0800

    feat(matrices): Rotate image iteration 1

--- bin/17_matrices/02_rotate_image.dart
+++ bin/17_matrices/02_rotate_image.dart
@@ -2,44 +2,21 @@ void main() {
   List<List<int>> rotateImage(List<List<int>> matrix) {
     final n = matrix.length;
 
-    var topLeft = [0, 0];
-    var topRight = [0, n - 1];
-    var bottomLeft = [n - 1, 0];
-    var bottomRight = [n - 1, n - 1];
+    var left = 0;
+    var right = n - 1;
 
-    while (topLeft.last < topRight.last && bottomLeft.first > topLeft.first) {
-      while (topLeft.last < topRight.last) {
-        var topLeftVal = matrix[topLeft.first][topLeft.last];
-        matrix[topLeft.first][topLeft.last] =
-            matrix[bottomLeft.first][bottomLeft.last];
-        var topRightVal = matrix[topRight.first][topRight.last];
-        matrix[topRight.first][topRight.last] = topLeftVal;
-        var bottomRightVal = matrix[bottomRight.first][bottomRight.last];
-        matrix[bottomRight.first][bottomRight.last] = topRightVal;
-        matrix[bottomLeft.first][bottomLeft.last] = bottomRightVal;
-
-        topLeft[1] = topLeft.last + 1;
-        topRight[0] = topRight.first + 1;
-        bottomRight[1] = bottomRight.last - 1;
-        bottomLeft[0] = bottomLeft.first - 1;
+    while (left < right) {
+      var top = left;
+      var bottom = right;
+      for (int i = 0; i < right - left; i++) {
+        var topLeftVal = matrix[top][left + i];
+        matrix[top][left + i] = matrix[bottom - i][left];
+        matrix[bottom - i][left] = matrix[bottom][right - i];
+        matrix[bottom][right - i] = matrix[top + i][right];
+        matrix[top + i][right] = topLeftVal;
       }
-
-      if (topLeft.last + 1 <= topRight.last &&
-          topLeft.first + 1 <= bottomLeft.first) {
-        topLeft[0] = topLeft.first + 1;
-      }
-      if (topRight.last - 1 >= topLeft.last &&
-          topRight.first + 1 <= bottomRight.first) {
-        topRight[1] = topRight.last - 1;
-      }
-      if (bottomRight.first - 1 >= topRight.first &&
-          bottomRight.last - 1 >= bottomLeft.last) {
-        bottomRight[0] = bottomRight.first - 1;
-      }
-      if (bottomLeft.first - 1 >= topLeft.first &&
-          bottomLeft.last + 1 <= topLeft.last) {
-        bottomLeft[1] = bottomLeft.last + 1;
-      }
+      left += 1;
+      right -= 1;
     }
     return matrix;
   }
