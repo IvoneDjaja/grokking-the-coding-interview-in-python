8717147d7ad65e828b8e66a4356618b08a181c5a
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-05-31 22:23:46 +0800

    feat(tree_dfs): Build binary tree from preorder and inorder traversal iteration 1

--- bin/20_tree_dfs/07_build_binary_tree_from_preorder_and_inorder_traversal.dart
+++ bin/20_tree_dfs/07_build_binary_tree_from_preorder_and_inorder_traversal.dart
@@ -15,25 +15,34 @@ void main() {
       inorderIndexMap[inorder[i]] = i;
     }
 
-    TreeNode? dfs(List<int> preorder, List<int> inorder) {
-      if (preorder.isEmpty || inorder.isEmpty) {
+    var preorderIndex = 0;
+    TreeNode? dfs(int start, int end) {
+      if (start > end) {
         return null;
       }
-      final data = preorder.removeAt(0);
+      final data = preorder[preorderIndex];
+      preorderIndex++;
+
+      final pivot = inorderIndexMap[data]!;
       final node = TreeNode(
         data: data,
-        left: dfs(preorder, inorder.sublist(0, inorderIndexMap[data]!)),
-        right: dfs(preorder, inorder.sublist(inorderIndexMap[data]!)),
+        left: dfs(start, pivot - 1),
+        right: dfs(pivot + 1, end),
       );
 
       return node;
     }
 
-    return dfs(preorder, inorder);
+    return dfs(0, n - 1);
   }
 
   /// CASE 1
   final preorder1 = [3, 9, 20, 15, 7];
   final inorder1 = [9, 3, 15, 20, 7];
-  print(buildTree(preorder1, inorder1));
+  print(buildTree(preorder1, inorder1)!.data);
+
+  /// CASE 2
+  final preorder2 = [10, 20, 40, 50, 30, 60];
+  final inorder2 = [40, 20, 50, 10, 60, 30];
+  print(buildTree(preorder2, inorder2)!.data);
 }
