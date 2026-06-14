b3931738192ff0a6682beca20b19675803ed9428
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-06-14 18:37:54 +0800

    feat(custom_data_structures): Implement lru cache iteration 0

---
+++ bin/26_custom_data_structures/03_implement_lru_cache.dart
@@ -0,0 +1,94 @@
+class LinkedListNode {
+  LinkedListNode({this.pair, this.next, this.prev});
+
+  List<int>? pair;
+  LinkedListNode? next;
+  LinkedListNode? prev;
+
+  int? get first => pair?.first;
+
+  int? get second => pair?.last;
+}
+
+class LRUCache {
+  LRUCache({required this.capacity});
+
+  final int capacity;
+  int length = 0;
+  LinkedListNode? root;
+
+  int get(int key) {
+    if (length == 0) {
+      return -1;
+    }
+    length -= 1;
+    int val = -1;
+    LinkedListNode? node;
+    if (root?.first == key) {
+      val = root?.second ?? -1;
+      node = root;
+      final next = root?.next;
+      root = next;
+      next?.prev = null;
+    } else {
+      var current = root;
+      while (current != null) {
+        if (current.first == key) {
+          val = current.second!;
+          node = current;
+          final prev = current.prev;
+          final next = current.next;
+          prev?.next = next;
+          next?.prev = prev;
+          break;
+        }
+        current = current.next;
+      }
+    }
+    var current = root;
+    if (root == null) {
+      root = node;
+    } else {
+      while (current?.next != null) {
+        current = current?.next;
+      }
+      current?.next = node;
+    }
+
+    return val;
+  }
+
+  void set(int key, int value) {
+    length += 1;
+    final node = LinkedListNode(pair: [key, value]);
+    if (root == null) {
+      root = node;
+      return;
+    }
+    var current = root;
+    while (current?.next != null) {
+      current = current?.next;
+    }
+    current?.next = node;
+    node.prev = current;
+
+    if (length > capacity) {
+      final next = root?.next;
+      root = next;
+      root?.prev = null;
+      length -= 1;
+    }
+  }
+}
+
+void main() {
+  /// CASE 1
+  final lruCache1 = LRUCache(capacity: 10);
+  lruCache1.set(2, 2);
+  lruCache1.set(3, 3);
+  lruCache1.set(4, 4);
+  lruCache1.set(5, 5);
+  print(lruCache1.get(1));
+  print(lruCache1.get(3));
+  print(lruCache1.get(2));
+}
