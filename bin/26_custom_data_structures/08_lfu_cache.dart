c542fc96984c7c5ad9e8c0dee9695ed15df11169
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-07-30 07:41:44 +0800

    feat(custom_data_structures): LFU cache iteration 1

--- bin/26_custom_data_structures/08_lfu_cache.dart
+++ bin/26_custom_data_structures/08_lfu_cache.dart
@@ -2,85 +2,112 @@ class Node {
   Node({required this.key, required this.val});
 
   final int key;
-  final int val;
+  int val;
   int useCounter = 0;
   Node? next;
   Node? prev;
 }
 
-class LFUCache {
-  LFUCache({required this.capacity})
+class DoublyLinkedList {
+  DoublyLinkedList()
     : start = Node(key: -1, val: -1),
       end = Node(key: -1, val: -1),
-      valMap = <int, Node>{},
-      countMap = <int, List<Node>>{} {
+      size = 0 {
     start.next = end;
     end.prev = start;
   }
 
   final Node start;
   final Node end;
+  int size;
+
+  void addToTail(Node node) {
+    final prev = end.prev;
+    prev!.next = node;
+    node.prev = prev;
+    node.next = end;
+    end.prev = node;
+    size += 1;
+  }
+
+  void remove(Node node) {
+    final next = node.next;
+    final prev = node.prev;
+    prev!.next = next;
+    next!.prev = prev;
+    size -= 1;
+  }
+
+  Node? removeHead() {
+    if (size == 0) {
+      return null;
+    }
+    final head = start.next;
+    remove(head!);
+    return head;
+  }
+}
+
+class LFUCache {
+  LFUCache({required this.capacity})
+    : keyMap = <int, Node>{},
+      countMap = <int, DoublyLinkedList>{};
+
   final int capacity;
-  final Map<int, Node> valMap;
-  final Map<int, List<Node>> countMap;
+  final Map<int, Node> keyMap;
+  final Map<int, DoublyLinkedList> countMap;
+  var minFrequency = 0;
 
   int get(int key) {
-    final hasKey = valMap.containsKey(key);
-    Node? node;
+    final hasKey = keyMap.containsKey(key);
 
     if (hasKey) {
-      node = valMap[key]!;
-      Node? current = end;
-      while (current?.prev != null) {
-        current = current?.prev;
-        if (current!.useCounter <= node.useCounter) {
-          node.useCounter += 1;
-          final next = current.next;
-          current.next = node;
-          node.prev = current;
-          node.next = next;
-          next!.prev = node;
-          break;
-        }
-      }
+      final node = keyMap[key]!;
+      _update(node);
+      return node.val;
     }
 
-    return node?.val ?? -1;
+    return -1;
+  }
+
+  void _update(Node node) {
+    var frequency = node.useCounter;
+    countMap[frequency]!.remove(node);
+    if (minFrequency == frequency && countMap[frequency]!.size == 0) {
+      minFrequency += 1;
+    }
+    node.useCounter += 1;
+    if (!countMap.containsKey(node.useCounter)) {
+      countMap[node.useCounter] = DoublyLinkedList();
+    }
+    countMap[node.useCounter]!.addToTail(node);
   }
 
   void put(int key, int val) {
-    late Node node;
-
-    final hasKey = valMap.containsKey(key);
+    if (capacity == 0) {
+      return;
+    }
+    final hasKey = keyMap.containsKey(key);
 
     if (hasKey) {
-      node = valMap[key]!;
+      final node = keyMap[key]!;
+      node.val = val;
+      _update(node);
     } else {
-      node = Node(key: key, val: val);
-    }
-
-    valMap[key] = node;
-    node.useCounter += 1;
-
-    Node? current = start;
-    while (current?.next != null) {
-      current = current?.next;
-      if (current!.useCounter > node.useCounter) {
-        final prev = current.prev;
-        prev?.next = node;
-        node.prev = prev;
-        node.next = current;
-        current.prev = node;
-        break;
+      if (keyMap.length + 1 > capacity) {
+        final deletedNode = countMap[minFrequency]?.removeHead();
+        if (deletedNode != null) {
+          keyMap.remove(deletedNode.key);
+        }
       }
-    }
-
-    if (valMap.length > capacity) {
-      final current = start.next;
-      final next = current?.next;
-      start.next = next;
-      next?.prev = start;
-      valMap.remove(current!.key);
+      final node = Node(key: key, val: val);
+      node.useCounter += 1;
+      keyMap[node.key] = node;
+      if (!countMap.containsKey(node.useCounter)) {
+        countMap[1] = DoublyLinkedList();
+      }
+      countMap[1]!.addToTail(node);
+      minFrequency = 1;
     }
   }
 }
@@ -92,4 +119,6 @@ void main() {
   cache1.put(2, 2);
   cache1.put(3, 3);
   print(cache1);
+  print(cache1.countMap);
+  print(cache1.keyMap);
 }
