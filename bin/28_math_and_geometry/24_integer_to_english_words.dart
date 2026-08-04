a961eae5f70205e4ba1569bfd0d5f52073c86c75
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-05 07:41:04 +0800

    feat(math_and_geometry): Integer to english words iteration 2

--- bin/28_math_and_geometry/24_integer_to_english_words.dart
+++ bin/28_math_and_geometry/24_integer_to_english_words.dart
@@ -1,55 +1,53 @@
 void main() {
+  final lessThanTwentyMap = <int, String>{
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen',
+  };
+
+  final tenMultiplesMap = <int, String>{
+    2: 'Twenty',
+    3: 'Thirty',
+    4: 'Torty',
+    5: 'Fifty',
+    6: 'Sixty',
+    7: 'Seventy',
+    8: 'Eighty',
+    9: 'Ninety',
+  };
+
+  final thousandMultiplesMap = <int, String>{
+    1000: 'Thousand',
+    1000000: 'Million',
+    1000000000: 'Billion',
+  };
   String numberToWords(int num) {
     var output = <String>[];
     var current = num;
 
-    final lessThanTwentyMap = <int, String>{
-      1: 'One',
-      2: 'Two',
-      3: 'Three',
-      4: 'Four',
-      5: 'Five',
-      6: 'Six',
-      7: 'Seven',
-      8: 'Eight',
-      9: 'Nine',
-      10: 'Ten',
-      11: 'Eleven',
-      12: 'Twelve',
-      13: 'Thirteen',
-      14: 'Fourteen',
-      15: 'Fifteen',
-      16: 'Sixteen',
-      17: 'Seventeen',
-      18: 'Eighteen',
-      19: 'Nineteen',
-    };
-
-    final tenMultiplesMap = <int, String>{
-      2: 'Twenty',
-      3: 'Thirty',
-      4: 'Torty',
-      5: 'Fifty',
-      6: 'Sixty',
-      7: 'Seventy',
-      8: 'Eighty',
-      9: 'Ninety',
-    };
-
-    final thousandMultiplesMap = <int, String>{
-      1000: 'Thousand',
-      1000000: 'Million',
-      1000000000: 'Billion',
-    };
-
-    String processLessThanThousand(int num) {
-      final output = <String>[];
+    void processLessThanThousand(int num) {
       var current = num;
       while (current > 0) {
         // less than 100
         if (current < 20) {
           output.add(lessThanTwentyMap[current]!);
-          current -= 20;
+          break;
         } else if (current < 100) {
           final quotient = current ~/ 10;
           output.add(tenMultiplesMap[quotient]!);
@@ -60,7 +58,6 @@ void main() {
           current %= 100;
         }
       }
-      return output.join(' ');
     }
 
     if (current == 0) {
@@ -73,7 +70,7 @@ void main() {
       final quotient = current ~/ divisor;
 
       if (quotient > 0) {
-        output.add(processLessThanThousand(quotient));
+        processLessThanThousand(quotient);
         if (divisor > 1) {
           output.add(thousandMultiplesMap[divisor]!);
         }
