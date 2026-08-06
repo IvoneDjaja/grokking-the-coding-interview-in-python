80ede051830e5dfb7808a7efdb116f535790173f
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-07 00:36:35 +0800

    feat(challenge_yourself): Longest palindromic substring iteration 1

--- 29_challenge_yourself/24_longest_palindromic_substring.py
+++ 29_challenge_yourself/24_longest_palindromic_substring.py
@@ -1,27 +1,32 @@
 def longest_palindromic_substring(s: str) -> str:
     n = len(s)
     i = 0
-    maxLength = 1
+    maxLength = 0
     startIndex = 0
-    endIndex = n
+    endIndex = startIndex + 1
     while i < n:
-        start = i - 1
-        end = i + 1
-        length = 1
-        while start > 0 and end < n:
-            if s[start] == s[end]:
-                length += 1
-            else:
-                break
+        start, end = i, i
+        while start > -1 and end < n and s[start] == s[end]:
+            if end - start + 1 > maxLength:
+                maxLength = end - start + 1
+                startIndex = start
+                endIndex = end
             start -= 1
             end += 1
-        if length > maxLength:
-            startIndex = start + 1
-            endIndex = end - 1
-        i = end
+        start, end = i, i + 1
+        while start > -1 and end < n and s[start] == s[end]:
+            if end - start + 1 > maxLength:
+                maxLength = end - start + 1
+                startIndex = start
+                endIndex = end
+            start -= 1
+            end += 1
+        i += 1
 
-    return s[startIndex:endIndex]
+    return s[startIndex:endIndex+1]
 
 # CASE 1
 input1 = 'asfdodfsaiuoefbwjebejwbf'
+input2 = 'aghsaashg'
 print(longest_palindromic_substring(input1))
+print(longest_palindromic_substring(input2))
