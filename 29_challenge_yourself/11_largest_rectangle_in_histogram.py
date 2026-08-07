ece1a7f45e994b9c0860821c023f11eccee568dd
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-07 21:19:37 +0800

    feat(challenge_yourself): Largest rectangle in histogram iteration 0 with failing input3

--- 29_challenge_yourself/11_largest_rectangle_in_histogram.py
+++ 29_challenge_yourself/11_largest_rectangle_in_histogram.py
@@ -1,15 +1,23 @@
-def largest_rectangle(arr: list[int]) -> int:
-    n = len(arr)
-    stack = []
+def largest_rectangle(heights: list[int]) -> int:
+    end = len(heights) - 1
+    start = 0
     maxArea = 0
-    i = 0
-    while i < n:
-        height = arr[i]
-        while len(stack) > 0 and arr[stack[-1][1]] > height:
-            prevInterval = stack.pop()
-            start = prevInterval[0]
-            end = prevInterval[1]
-            prevHeight = arr[end]
-            maxArea = max(maxArea, prevHeight * (i - end))
-            if arr[i] > 0:
-                stack.append([i , i])
+    while start <= end:
+        startHeight = heights[start]
+        endHeight = heights[end]
+        minHeight = min(startHeight, endHeight)
+        area = minHeight * (end + 1 - start)
+        maxArea = max(maxArea, area)
+        if startHeight < endHeight:
+            start += 1
+        else:
+            end -= 1
+
+    return maxArea
+
+input1 = [1, 3, 4, 2, 2]
+print(largest_rectangle(input1))
+input2 = [2, 4, 5, 6, 3]
+print(largest_rectangle(input2))
+input3 = [6,2,5,4,5,1,6,4,2]
+print(largest_rectangle(input3))
