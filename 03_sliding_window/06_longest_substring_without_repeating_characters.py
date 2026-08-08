b6cd889db119315e1c431d096bc9fbe226ad4645
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-08 15:34:54 +0800

    feat(sliding_window): Longest substring without repeating characters iteration 1

--- 03_sliding_window/06_longest_substring_without_repeating_characters.py
+++ 03_sliding_window/06_longest_substring_without_repeating_characters.py
@@ -8,7 +8,7 @@ def find_longest_substring(input_str):
          letter_map[letter] = 0
       letter_map[letter] += 1
       while letter_map[letter] > 1:
-         letter_map[letter] -= 1
+         letter_map[input_str[start]] -= 1
          start += 1
       max_length= max(max_length, i + 1 - start)
    return max_length
