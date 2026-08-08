007ef5c24ce02be7aabef2b248ef9115fb0fb31a
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-08 16:38:32 +0800

    feat(sliding_window): Longest repeating character replacement iteration 1

--- 03_sliding_window/04_longest_repeating_character_replacement.py
+++ 03_sliding_window/04_longest_repeating_character_replacement.py
@@ -4,11 +4,13 @@ def longest_repeating_character_replacement(s: list[str], k: int) -> int:
 	max_length = 0
 	n = len(s)
 	start = 0
+	max_freq = 0
 	for i in range(n):
 		char = s[i]
-		count = char_map.get(char, 0)
-		char_map[char] = count + 1
-		while i + 1 - char_map[s[start]] - start > k:
+		count = char_map.get(char, 0) + 1
+		char_map[char] = count
+		max_freq = max(max_freq, count)
+		while i + 1 - start - max_freq > k:
 			char_map[s[start]] -= 1
 			start += 1
 		max_length = max(max_length, i + 1 - start)
