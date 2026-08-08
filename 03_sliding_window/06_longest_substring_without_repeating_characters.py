8dbbbf3c75e58dfc9437dbbb3e549860a4f4be94
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-08 15:34:32 +0800

    feat(sliding_window): Longest substring without repeating characters iteration 0 with failing case input1

---
+++ 03_sliding_window/06_longest_substring_without_repeating_characters.py
@@ -0,0 +1,17 @@
+def find_longest_substring(input_str):
+   letter_map = {}
+   start = 0
+   max_length = 0
+   for i in range(len(input_str)):
+      letter = input_str[i]
+      if letter not in letter_map:
+         letter_map[letter] = 0
+      letter_map[letter] += 1
+      while letter_map[letter] > 1:
+         letter_map[letter] -= 1
+         start += 1
+      max_length= max(max_length, i + 1 - start)
+   return max_length
+
+input1 = 'pwwkew'
+print(find_longest_substring(input1))
