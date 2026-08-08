a8efabecd315da44da4cae2fcce4fe51f0bb5784
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-08 18:49:17 +0800

    feat(sliding_window): Minimum window substring iteration 1

--- 03_sliding_window/05_minimum_window_substring.py
+++ 03_sliding_window/05_minimum_window_substring.py
@@ -2,12 +2,14 @@ def min_window(s: str, t: str) -> str:
     if len(s) < len(t):
         return ''
 
+    if len(s) < len(t):
+            return ''
+
     t_map = {}
     s_map = {}
     min_length = float('inf')
     start_index = 0
     end_index = 0
-    t_count = 0
 
     for char in t:
         count = t_map.get(char, 0) + 1
@@ -15,24 +17,27 @@ def min_window(s: str, t: str) -> str:
 
     n = len(s)
     start = 0
+    have = 0
+    need = len(t_map)
     for i in range(n):
         char = s[i]
         count = s_map.get(char, 0) + 1
         s_map[char] = count
-        if char in t_map:
-            t_count += 1
+
+        if char in t_map and s_map[char] == t_map[char]:
+            have += 1
         
-        while t_count >= len(t):
-            if s[start] in t_map and t_count == len(t):
-                break
-            s_map[s[start]] -= 1
-            if s[start] in t_map:
-                t_count -= 1
+        while have == need:
+            if min_length > i + 1 - start:
+                min_length = i + 1 - start
+                start_index = start
+                end_index = i + 1
+            start_char = s[start]
+            s_map[start_char] -= 1
+            if start_char in t_map and s_map[start_char] < t_map[start_char]:
+                have -= 1
             start += 1
-        if t_count == len(t) and min_length > i + 1 - start:
-            min_length = i + 1 - start
-            start_index = start
-            end_index = i + 1
+
     return s[start_index:end_index]
 
 s1 = 'cbac'
@@ -42,3 +47,7 @@ print(min_window(s1, t1))
 s2 = 'aab'
 t2 = 'ab'
 print(min_window(s2, t2))
+
+s3="aaaaaaaaaaaabbbbbcdd"
+t3="abcdd"
+print(min_window(s3, t3))
