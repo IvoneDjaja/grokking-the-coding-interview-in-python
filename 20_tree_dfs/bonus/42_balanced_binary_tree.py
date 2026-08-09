3840b8ccb98b78284381c24efad74572546a312e
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-09 09:44:49 +0800

    feat(tree_dfs): Balanced binary tree iteration 2

--- 20_tree_dfs/bonus/42_balanced_binary_tree.py
+++ 20_tree_dfs/bonus/42_balanced_binary_tree.py
@@ -5,14 +5,14 @@ class TreeNode:
         self.right = None
 
 def balanced_binary_tree(root: TreeNode) -> bool:
-	return dfs(root)[1]
+	return dfs(root) != -1
 
 def dfs(root: TreeNode) -> TreeNode:
     if not root:
-        return [0, True]
-    left_result = dfs(root.left)
-    right_result = dfs(root.right)
-    left_height = left_result[0]
-    right_height = right_result[0]
-    result = abs(left_height - right_height) <= 1
-    return [1 + max(left_height, right_height), left_result[1] and right_result[1] and result]
+        return 0
+    left_height = dfs(root.left)
+    right_height = dfs(root.right)
+    if left_height == -1 or right_height == -1:
+         return -1
+    isBalanced = abs(left_height - right_height) <= 1
+    return 1 + max(left_height, right_height) if isBalanced else -1
