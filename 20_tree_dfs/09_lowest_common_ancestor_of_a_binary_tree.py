810d331afe93071bc8b27189a1ba4731bf722f75
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-09 15:06:10 +0800

    feat(tree_dfs): Lowest common ancestor of a binary search tree iteration 2

--- 20_tree_dfs/09_lowest_common_ancestor_of_a_binary_tree.py
+++ 20_tree_dfs/09_lowest_common_ancestor_of_a_binary_tree.py
@@ -4,14 +4,14 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def lowest_common_ancestor(self, current_node, p, q):
+def lowest_common_ancestor(current_node, p, q):
     if not current_node or current_node == p or current_node == q:
         return current_node
     
-    left = self.lowest_common_ancestor(current_node.left, p, q)
-    right = self.lowest_common_ancestor(current_node.right, p, q)
+    left = lowest_common_ancestor(current_node.left, p, q)
+    right = lowest_common_ancestor(current_node.right, p, q)
     
-    if left and right:
+    if lowest_common_ancestor(current_node.left, p, q) and lowest_common_ancestor(current_node.right, p, q):
         return current_node
 
-    return left if left else right
+    return left or right
