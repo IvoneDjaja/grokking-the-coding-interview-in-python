6a3da15b796f527c9c36cb04d8a9694ef0f4eaa7
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-09 09:55:39 +0800

    feat(tree_dfs): Same tree iteration 1

---
+++ 20_tree_dfs/bonus/33_same_tree.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def same_binary_tree(root1: TreeNode, root2: TreeNode) -> TreeNode:
+	if not root1 and not root2:
+		return True
+	if not root1 and root2:
+		return False
+	if not root2 and root1:
+		return False
+
+	if root1.data != root2.data:
+		return False
+
+	return same_binary_tree(root1.left, root2.left) and same_binary_tree(root1.right, root2.right)
