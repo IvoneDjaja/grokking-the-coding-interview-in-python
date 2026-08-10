9dd3542d85fc104adc4f17921d92310daec3957e
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-10 09:28:42 +0800

    feat(dynamic_programming): House robber iteration 2

--- 13_dynamic_programmic/bonus/30_house_robber.py
+++ 13_dynamic_programmic/bonus/30_house_robber.py
@@ -3,14 +3,10 @@ from typing import List
 
 def houseRobber(nums: List[int]) -> int:
 	n = len(nums)
-	if len(nums) < 2:
-		return nums[0]
-	if len(nums) == 2:
-		return max(nums[0], nums[1])
-
-	first = nums[0]
-	second = max(nums[0], nums[1])
-	for i in range(2, n):
-		first, second = second, max(second, nums[i] + first)
+	first = 0
+	second = 0
+	for i in range(n):
+		temp = max(second, first + nums[i])
+		first, second = second, temp
 
 	return second
