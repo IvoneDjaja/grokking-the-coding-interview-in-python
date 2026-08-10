94b238b2b03b3b1a1cc464d97a93384da2c72ba0
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-10 09:55:22 +0800

    feat(dynamic_programming): House robber 2 iteration 0

---
+++ 13_dynamic_programmic/07_house_robber_2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def houseRobber2(nums: List[int]) -> int:
+	n = len(nums)
+	first = 0
+	second = 0
+	for i in range(n - 1):
+		temp = max(second, first + nums[i])
+		first, second = second, temp
+	maxFirst = second
+
+	first = 0
+	second = 0
+	for i in range(1, n + 1):
+		temp = max(second, first + nums[i])
+		first, second = second, temp
+	maxSecond = second
+
+	return max(maxFirst, maxSecond)
