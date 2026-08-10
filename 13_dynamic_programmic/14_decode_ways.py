939a68186e285faf9414dc5df2682380d0e4bff1
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-10 15:05:47 +0800

    feat(dynamic_programming): Decode ways iteration 0

---
+++ 13_dynamic_programmic/14_decode_ways.py
@@ -0,0 +1,17 @@
+def decodeWays(s: str) -> int:
+    n = len(s)
+    if n == 0 or s[0] == '0':
+        return 0
+    
+    memo = (n + 1) * [0]
+    memo[0] = 1
+    memo[1] = 1
+    for i in range(2, n):
+        singleDigit = int(s[i])
+        if 1 <= singleDigit <= 9:
+            memo[i] += memo[i - 1]
+        doubleDigit = int(s[i-2: i])
+        if 10 <= doubleDigit <= 26:
+            memo[i] += memo[i - 2]
+    return memo[n]
+        
