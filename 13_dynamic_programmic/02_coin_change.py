5baea9b614f752e5638b61b496562ade0700c5cf
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-10 16:23:18 +0800

    feat(dynamic_programming): Coin change iteration 2

--- 13_dynamic_programmic/02_coin_change.py
+++ 13_dynamic_programmic/02_coin_change.py
@@ -1,11 +1,13 @@
 def coinChange(coins: List[int], amount: int) -> int:
 	n = amount
 	memo = (n + 1) * [float('inf')]
+	coins.srot()
 
 	memo[0] = 0
 	for i in range(1, n + 1):
 		for coin in coins:
-			if i - coin >= 0:
-				memo[i] = min(memo[i], memo[i-coin] + 1)
+			if i - coin < 0:
+				break
+			memo[i] = min(memo[i], memo[i-coin] + 1)
 
 	return -1 if memo[n] == float('inf') else memo[n]
