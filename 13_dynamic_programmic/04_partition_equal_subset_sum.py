500478f6218ed2b9fdde58ca688400b714d73786
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-10 19:52:27 +0800

    feat(dynamic_programming): Partition equal subset sum iteration 1

---
+++ 13_dynamic_programmic/04_partition_equal_subset_sum.py
@@ -0,0 +1,15 @@
+def canPartition(nums: List[int]) -> bool:
+    total = sum(nums)
+
+    if total % 2 != 0:
+        return False
+
+    target = total // 2
+    dp = (target + 1) * [0]
+    dp[0] = True
+
+    for num in nums:
+        for j in range(target, num-1, -1):
+            dp[j] = dp[j] or dp[j - num]
+
+    return dp[num]
