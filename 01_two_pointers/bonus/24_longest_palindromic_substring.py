2b0e502eafa859c74698cad14d0c0152e69ff002
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-10 12:37:44 +0800

    feat(challenge_yourself): Longest palindromic substring iteration 2

--- 01_two_pointers/bonus/24_longest_palindromic_substring.py
+++ 01_two_pointers/bonus/24_longest_palindromic_substring.py
@@ -1,32 +1,23 @@
-def longest_palindromic_substring(s: str) -> str:
+def longestPalindrome(s: str) -> int:
     n = len(s)
-    i = 0
     maxLength = 0
     startIndex = 0
-    endIndex = startIndex + 1
-    while i < n:
-        start, end = i, i
-        while start > -1 and end < n and s[start] == s[end]:
-            if end - start + 1 > maxLength:
-                maxLength = end - start + 1
-                startIndex = start
-                endIndex = end
-            start -= 1
-            end += 1
-        start, end = i, i + 1
-        while start > -1 and end < n and s[start] == s[end]:
-            if end - start + 1 > maxLength:
-                maxLength = end - start + 1
-                startIndex = start
-                endIndex = end
-            start -= 1
-            end += 1
-        i += 1
 
-    return s[startIndex:endIndex+1]
+    def expand(start, end):
+        while start >= 0 and end < n and s[start] == s[end]:
+            start -= 1
+            end += 1
+        return [start + 1, end - 1]
+    for i in range(n):
+        for start, end in (expand(i, i), expand(i, i + 1)):
+            if (end + 1 - start) > maxLength:
+                maxLength = end - start + 1
+                startIndex = start
+    return s[startIndex:startIndex+maxLength]
+
 
 # CASE 1
 input1 = 'asfdodfsaiuoefbwjebejwbf'
 input2 = 'aghsaashg'
-print(longest_palindromic_substring(input1))
-print(longest_palindromic_substring(input2))
+print(longestPalindrome(input1))
+print(longestPalindrome(input2))
