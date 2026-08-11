c26e94f5ff1ae0b8131b20f19598af5bec2e979c
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-11 21:51:53 +0800

    feat(dynamic_programming): Target sum iteration 1 with top down manual memoization

--- 13_dynamic_programmic/2d/49_target_sum.py
+++ 13_dynamic_programmic/2d/49_target_sum.py
@@ -1,9 +1,14 @@
 def target_sum(nums: List[int], target: int):
+    memo = {}
 	
     def dfs(index, total):
+        if index == len(nums):
+            return 1 if total == target else 0
+        state = (index, total)
+        if state in memo:
+            return memo[state]
         num = nums[index]
-        if total == target:
-            return 1 if index >= len(nums) else 0
-        return dfs(index + 1, total + num) + dfs(index + 1, total - num)
+        memo[state] = dfs(index + 1, total + num) + dfs(index + 1, total - num)
+        return memo[state]
 
     return dfs(0, 0)
