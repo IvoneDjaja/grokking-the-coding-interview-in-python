2445dbf595183e7388cc05b38a87935df1f761d4
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-11 19:40:30 +0800

    feat(dynamic_programming): Unique paths iteration 1

--- 13_dynamic_programmic/2d/bonus/unique_paths.py
+++ 13_dynamic_programmic/2d/bonus/unique_paths.py
@@ -1,6 +1,6 @@
 def uniquePaths(m: int, n: int) -> int:
-	dp = n * [1]
+	dp = [n * [1] for _ in range(m)]
 	for i in range(1, m):
 		for j in range(1, n):
-			dp[j] = dp[j] + dp[j-1]
-	return dp[n-1]
+			dp[i][j] = dp[i-1][j] + dp[j][j-1]
+	return dp[m-1][n-1]
