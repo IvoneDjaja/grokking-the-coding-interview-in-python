a04cb6fc59ecdc02c43ae71fc49d175486b58814
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-11 23:56:15 +0800

    feat(dynamic_programming): Target sum iteration 1 with bottom up 2d dp

--- 13_dynamic_programmic/2d/49_target_sum.py
+++ 13_dynamic_programmic/2d/49_target_sum.py
@@ -1,14 +1,12 @@
-def target_sum(nums: List[int], target: int):
-    memo = {}
-	
-    def dfs(index, total):
-        if index == len(nums):
-            return 1 if total == target else 0
-        state = (index, total)
-        if state in memo:
-            return memo[state]
-        num = nums[index]
-        memo[state] = dfs(index + 1, total + num) + dfs(index + 1, total - num)
-        return memo[state]
+from collections import defaultdict
 
-    return dfs(0, 0)
+
+def target_sum(nums: List[int], target: int):
+    dp = {0: 1}
+    for num in nums:
+        dp_next = defaultdict(int)
+        for total, count in dp.items():
+            dp[total + num] += 1
+            dp[total - num] += 1
+        dp = dp_next
+    return dp[target]
