5a825bf147050592c3f035aae7b78de36b8ab868
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-11 19:57:37 +0800

    feat(dynamic_programming): Coin change 2 iteration 1

---
+++ 13_dynamic_programmic/2d/coin_change_2.py
@@ -0,0 +1,15 @@
+def coin_change_2(coins: List[int], amount: int) -> int:
+    n = len(coins)
+    dp = [[0] * (amount + 1) for _ in range(n + 1)]
+
+    for i in range(n):
+        dp[i + 1][0] = 1
+
+    for i in range(1, n + 1):
+        coin = coins[i - 1]
+        for j in range(1, amount + 1):
+            dp[i][j] = dp[i - 1][j]
+            if j - coin >= 0:
+                dp[i][j] += dp[i][j - coin]
+
+    return dp[n][amount]
