51b5460fa2eb748b99708e4b8f7c456236ce9122
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-14 16:47:01 +0800

    feat(intervals): Find median from a data stream iteration 2

--- 06_two_heaps/02_find_median_from_a_data_stream.py
+++ 06_two_heaps/02_find_median_from_a_data_stream.py
@@ -3,28 +3,15 @@ class MedianFinder:
     def __init__(self):
         self.maxHeap = []
         self.minHeap = []
-        self.length = 0
-        heapq.heapify(self.minHeap)
-        heapq.heapify(self.maxHeap)
 
     def addNum(self, num: int) -> None:
-        self.length += 1
+        val = -heapq.heappushpop(self.maxHeap, -num)
+        heapq.heappush(self.minHeap, val)
 
-        if self.minHeap and num > self.minHeap[0]:
-            minRight = heapq.heappop(self.minHeap)
-            heapq.heappush(self.minHeap, num)
-            heapq.heappush(self.maxHeap, -minRight)
-        else:
-            heapq.heappush(self.maxHeap, -num)
-
-        while len(self.maxHeap) - len(self.minHeap) > 1:
-            maxLeft = -heapq.heappop(self.maxHeap)
-            heapq.heappush(self.minHeap, maxLeft)
+        if len(self.minHeap) > len(self.maxHeap):
+            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
 
     def findMedian(self) -> float:
-        maxLeft = -self.maxHeap[0]
-        if self.length % 2 != 0:
-            return maxLeft
-        elif self.minHeap:
-            maxRight = self.minHeap[0]
-            return (maxLeft + maxRight) / 2
+        if len(self.maxHeap) > len(self.minHeap):
+                return -self.maxHeap[0]
+        return (-self.maxHeap[0] + self.minHeap[0]) / 2
