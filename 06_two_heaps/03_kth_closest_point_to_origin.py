f425dea47a57b0b51e3538208db8b5c2801b7688
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2026-08-14 14:10:16 +0800

    feat(top_k_elements): Kth closest points to origin iteration 1

---
+++ 06_two_heaps/03_kth_closest_point_to_origin.py
@@ -0,0 +1,14 @@
+def kthClosestPointsToOrigin(points, k):
+	minHeap = []
+	for i, j in points:
+		distance = math.sqrt(i ** 2 + j ** 2)
+		minHeap.append([distance, (i, j)])
+	heapq.heapify(minHeap)
+
+	n = len(minHeap)
+	output = []
+	while k > 0 and len(minHeap) > 0:
+		output.append(heapq.heappop(minHeap))
+		k -= 1
+		
+	return output
